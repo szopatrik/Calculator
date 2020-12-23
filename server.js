0437004dcc5028acc7b3176a6ff527a1859a233f
Patrik Szó <patrik.szo@livesportnews.eu>
2020-12-23 19:39:14 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", () => {
+    main();
+}
+);
+
+function main(){
+    //Deklarace proměnných
+    const prvniCislo = document.getElementById("prvnicislo");
+    const druheCislo = document.getElementById("druhecislo");
+    const minus = document.getElementById("minus");
+    const plus = document.getElementById("plus");
+
+    const nasobeni = document.getElementById("nasobeni");
+    const deleni = document.getElementById("deleni");
+    const reset = document.getElementById("reser");
+    const vysledek = document.getElementById("vysledek");
+
+    //navazani klikacích eventů na  tlačítka
+}
+    
+    
+
+    
